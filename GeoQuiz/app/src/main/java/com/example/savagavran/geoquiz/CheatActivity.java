2f364bfa524453e5533bc30911a597fbc78399d0
sava.gavran <gavransava@gmail.com>
2016-10-07 18:41:50 +0200

    GeoQuiz - Animation (compatibility issue) + last challenge

--- GeoQuiz/app/src/main/java/com/example/savagavran/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/example/savagavran/geoquiz/CheatActivity.java
@@ -1,13 +1,19 @@
 package com.example.savagavran.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class CheatActivity extends AppCompatActivity {
 
@@ -17,6 +23,7 @@ public class CheatActivity extends AppCompatActivity {
             "com.example.savagavran.geoquiz.answer_shown";
     private boolean mAnswerIsTrue;
     private TextView mAnswerTextView;
+    private TextView mApiLevel;
     private Button mShowAnswer;
     private boolean isAnswerShown;
 
@@ -59,10 +66,35 @@ public class CheatActivity extends AppCompatActivity {
                 } else {
                     mAnswerTextView.setText(R.string.false_button);
                 }
-                isAnswerShown = true;
-                setAnswerShownResult(isAnswerShown);
+
+                setAnswerShownResult(isAnswerShown = true);
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator anim = ViewAnimationUtils
+                            .createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mAnswerTextView.setVisibility(View.VISIBLE);
+                            mShowAnswer.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                }
+                else {
+                    mAnswerTextView.setVisibility(View.VISIBLE);
+                    mShowAnswer.setVisibility(View.INVISIBLE);
+
+                }
             }
         });
+
+        mApiLevel = (TextView) findViewById(R.id.device_api_level);
+        String apiLevel = getString(R.string.api_level)+ " " + Integer.toString(Build.VERSION.SDK_INT);
+        mApiLevel.setText(apiLevel);
     }
 
     @Override
