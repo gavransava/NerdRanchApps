7d4d43a724b6c85243fba6cf982a92a832c0abde
sava.gavran <gavransava@gmail.com>
2016-10-10 18:46:59 +0200

    CriminalIntent - Add MVC classes with fragment and basic functionality

--- CriminalIntent/app/src/main/java/com/example/savagavran/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/com/example/savagavran/criminalintent/CrimeActivity.java
@@ -1,13 +1,28 @@
 package com.example.savagavran.criminalintent;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class CrimeActivity extends AppCompatActivity {
+public class CrimeActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
