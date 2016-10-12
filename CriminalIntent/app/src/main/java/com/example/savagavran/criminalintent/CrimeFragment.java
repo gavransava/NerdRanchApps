f788f74fa05ce77adf23afe2801de3939e6bf1a9
sava.gavran <gavransava@gmail.com>
2016-10-12 16:21:42 +0200

    CriminalIntent - Date Formating + Land layout

--- CriminalIntent/app/src/main/java/com/example/savagavran/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/example/savagavran/criminalintent/CrimeFragment.java
@@ -1,5 +1,6 @@
 package com.example.savagavran.criminalintent;
 
+import java.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
@@ -7,7 +8,11 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+
 
 /**
  * Created by sava.gavran on 10/10/2016.
@@ -17,6 +22,39 @@ public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
+    private final String DATE_FORMAT = "EEEE, MMM d, y";
+
+    private void wireTitleField() {
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s){}
+        });
+    }
+
+    private void wireDateButton() {
+        SimpleDateFormat fmt = new SimpleDateFormat(DATE_FORMAT);
+        mDateButton.setText(fmt.format(mCrime.getDate()));
+        mDateButton.setEnabled(false);
+    }
+
+    private void wireSolvedCheckBox() {
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,24 +65,13 @@ public class CrimeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
-
         mTitleField = (EditText)v.findViewById(R.id.crime_title);
-        mTitleField.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
 
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                mCrime.setTitle(s.toString());
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
+        wireTitleField();
+        wireDateButton();
+        wireSolvedCheckBox();
 
         return v;
     }
