7372dfe248b1667872fba85155bc172c54bb0701
sava.gavran <gavransava@gmail.com>
2016-10-28 15:35:53 +0200

    BeatBox - GridLayout/RecyclerView

---
+++ BeatBox/app/src/main/java/com/example/savagavran/beatbox/BeatBoxActivity.java
@@ -0,0 +1,11 @@
+package com.example.savagavran.beatbox;
+
+import android.support.v4.app.Fragment;
+
+
+public class BeatBoxActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
+    }
+}
