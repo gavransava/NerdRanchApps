95d604bd1aafe3df67263a6a133b4626419e44c7
sava.gavran <gavransava@gmail.com>
2016-10-31 16:16:36 +0100

    PhotoGallery - URl request and json parsing.

---
+++ PhotoGallery/app/src/main/java/com/myexamples/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,18 @@
+package com.myexamples.photogallery;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    @Override
+    public Fragment createFragment() {
+        return PhotoGalleryFragment.newInstance();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+    }
+}
